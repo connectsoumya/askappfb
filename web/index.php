e67a59894bc4c2518f57a31f4c47385841031a9e
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-03-30 19:23:50 +0200

    make it better

--- web/index.php
+++ web/index.php
@@ -1,31 +1,39 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
-
 <?php
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->post('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello, help me to make this app';
-  echo file_get_contents("index.html");
-});
-
-$app->run();
-
-
- </body>
+$myOutput = <<<MYHTMLSAFEOUTPUT
+<?xml version="1.0"?>
+<html>
+  <title>PHP Example</title>
+  <body>
+    <p>...all sorts goes here...</p>
+  </body>
 </html>
+MYHTMLSAFEOUTPUT;
+
+echo $myOutput; 
+
+// require('../vendor/autoload.php');
+
+// $app = new Silex\Application();
+// $app['debug'] = true;
+
+// // Register the monolog logging service
+// $app->register(new Silex\Provider\MonologServiceProvider(), array(
+//   'monolog.logfile' => 'php://stderr',
+// ));
+
+// // Our web handlers
+
+// $app->post('/', function() use($app) {
+//   $app['monolog']->addDebug('logging output.');
+//   return 'Hello, help me to make this app';
+  
+// });
+
+// $app->run();
+
+
+//  </body>
+// </html>
+
+?>
