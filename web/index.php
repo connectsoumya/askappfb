3177f75c4bec7233ef817b5771dd76b0c6809ad8
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-02 18:33:35 +0200

    make it better

--- web/index.php
+++ web/index.php
@@ -16,20 +16,11 @@ $myOutput = <<<MYHTMLSAFEOUTPUT
       version    : 'v2.3'
     });
 
-// // modification starts here
-//   <p id="demo">Demo</p>
-//   function onLogin(response) {  
-//     FB.api('me?fields=posts.limit(2){message}', function(qstn) {
-//       var question = document.getElementById('demo');
-//       question.innerHTML = 'qstn.posts.limit(2){message}';
-//   });
-// }
 
-// // modification ends here
 
-//     FB.login(function(){
-//  		FB.api('/1608260672741284/feed', 'post', {message: 'question.innerHTML', access_token: 'CAAZAatKCQfR0BAFPZA2iybnKTxlZBZBspYyGHfvdZB0T10KrD0KSiJ1y8PUkp527stTYCPCnP3nUa0UaSOqZClqpPXneL1gwFZAgF4cV8kGCKg0NoZCE8YHLvTN2nBdjBFeBqnATDt45ghZCj6T0Jk4fOnbM1h01whvdEpLZBajkjxiFGruVIeIHq9MmZCFRRfWmSMZD'});
-// 		}, {scope: 'publish_actions'});
+    FB.login(function(){
+ 		FB.api('/1608260672741284/feed', 'post', {message: 'question.innerHTML', access_token: 'CAAZAatKCQfR0BAFPZA2iybnKTxlZBZBspYyGHfvdZB0T10KrD0KSiJ1y8PUkp527stTYCPCnP3nUa0UaSOqZClqpPXneL1gwFZAgF4cV8kGCKg0NoZCE8YHLvTN2nBdjBFeBqnATDt45ghZCj6T0Jk4fOnbM1h01whvdEpLZBajkjxiFGruVIeIHq9MmZCFRRfWmSMZD'});
+		}, {scope: 'publish_actions'});
 
     function onLogin(response) {
   		if (response.status == 'connected') {
@@ -66,24 +57,6 @@ FB.getLoginStatus(function(response) {
    }(document, 'script', 'facebook-jssdk'));
 </script>
 
-  <p id="demo">Demo</p>
-
-<script>
-// modification starts here
-
-  function onLogin(response) {  
-    FB.api('me?fields=posts.limit(2){message}', function(qstn) {
-      var question = document.getElementById('demo');
-      question.innerHTML = 'qstn.posts.limit(2){message}';
-  });
-}
-
-// modification ends here
-
-    FB.login(function(){
-    FB.api('/1608260672741284/feed', 'post', {message: 'question.innerHTML', access_token: 'CAAZAatKCQfR0BAFPZA2iybnKTxlZBZBspYyGHfvdZB0T10KrD0KSiJ1y8PUkp527stTYCPCnP3nUa0UaSOqZClqpPXneL1gwFZAgF4cV8kGCKg0NoZCE8YHLvTN2nBdjBFeBqnATDt45ghZCj6T0Jk4fOnbM1h01whvdEpLZBajkjxiFGruVIeIHq9MmZCFRRfWmSMZD'});
-    }, {scope: 'publish_actions'});
-</script>
 
 // <h1>Publishing to the Graph API</h1>
 
