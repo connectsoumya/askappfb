0f770223b9ce555db9ab40c0ba3b8966f1adb225
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-08 23:12:24 +0200

    make it better

--- web/extract_details.php
+++ web/extract_details.php
@@ -1,5 +1,12 @@
-<?php
-$data = file_get_contents("https://graph.facebook.com/1608260672741284/feed?fields=message,comments,likes&access_token=CAAZAatKCQfR0BAO8JaBcFJXRns5whf4CyNhjA6EJ1tWwUSPOTLB99XldBMwXJOXpAsIIvHOjhkKMnuo4dpkNJMZBgvZBkEHqK3EIReb3vJoyavKYZBBxG5OuddAwo48jTo6HF0ieSX3XEzrZAqGSRLemMJxtV2SNYCoGfvR6UmmmZCoNanU6rf1YjZCH0vv3dBwixAyFR5lsAZDZD");
+<? php
+
+$url = $_POST['https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BANDfu63WDp5q4ybS9GG5ZCAIjXt7BxVwbP3KIOSpDt4Qf5BTZAm8tIB5fiB5O8qa8IRNwkW7NopLEUw16nrSpNdLCgZClGWqQn2yma4tZBq7fj2ZAAbP9UYH835QZBgeIRRZBgS255l8n0ZCaqFixIO6ohKukWx5l92tm8OaAZBzeP6xZCLvUAZAt4ZD'];
+$token = file_get_contents($url);
+$fbinit="https://graph.facebook.com/1608260672741284/feed?fields=message,comments,likes&";
+$result = $fbinit . $token;
+$data = file_get_contents($result);
 file_put_contents("details.json",$data);
 print_r( json_decode($data, true) );
+echo $data
+
 ?>
