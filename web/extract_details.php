83a503ea58b647257392c70aa3b346f09d04cd9a
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-08 23:51:55 +0200

    make it better

--- web/extract_details.php
+++ web/extract_details.php
@@ -7,6 +7,7 @@
 <html>
   <title>PHP Example</title>
   <body>
+  jjhjhjjhjhjhjhjhjhjhjhjhjhjh
     <? php
 echo "hi!";
 $url = $_GET['https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BANDfu63WDp5q4ybS9GG5ZCAIjXt7BxVwbP3KIOSpDt4Qf5BTZAm8tIB5fiB5O8qa8IRNwkW7NopLEUw16nrSpNdLCgZClGWqQn2yma4tZBq7fj2ZAAbP9UYH835QZBgeIRRZBgS255l8n0ZCaqFixIO6ohKukWx5l92tm8OaAZBzeP6xZCLvUAZAt4ZD'];
