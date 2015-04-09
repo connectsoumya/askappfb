0f82f68ad9c7fdf311df5cc5340090d6fe1fec9c
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-09 11:44:30 +0200

    make it better

--- web/extract_details.php
+++ web/extract_details.php
@@ -7,17 +7,15 @@
 <html>
   <title>PHP Example</title>
   <body>
- <p> jjhjhjjhjhjhjhjhjhjhjhjhjhjh </p>
-    <? php
-echo "hi!";
-$url = $_GET['https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BANDfu63WDp5q4ybS9GG5ZCAIjXt7BxVwbP3KIOSpDt4Qf5BTZAm8tIB5fiB5O8qa8IRNwkW7NopLEUw16nrSpNdLCgZClGWqQn2yma4tZBq7fj2ZAAbP9UYH835QZBgeIRRZBgS255l8n0ZCaqFixIO6ohKukWx5l92tm8OaAZBzeP6xZCLvUAZAt4ZD'];
-$token = file_get_contents($url);
-$fbinit="https://graph.facebook.com/1608260672741284/feed?fields=message,comments,likes&";
-$result = $fbinit . $token;
-$data = file_get_contents($result);
-file_put_contents("details.json",$data);
-print_r( json_decode($data, true) );
-echo $data
+<? php
+	$url = $_GET['https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAACEdEose0cBAGA9omoAxZCcW7iy0ChfErDN4f5Izl4xrtmG01Tad6V8fRZAdPb4YqC4iRLuSdSio92BdFeyTI5U8B8rx5BmuUgBhll8wkoQFlfYKxLkr5FVIoPrPZCjINVaJzXDZAQsGwqsfmq4xHrr7LJKaH9blUtXTyPdhxc5FTubQcssxeO8sdhZBqMfno7uAix0ZCdwZDZD'];
+	$token = file_get_contents($url);
+	$fbinit="https://graph.facebook.com/1608260672741284/feed?fields=message,comments,likes&";
+	$result = $fbinit . $token;
+	$data = file_get_contents($result);
+	file_put_contents("details.json",$data);
+	print_r( json_decode($data, true) );
+	echo $data
 ?>
   </body>
 </html>
--- web/index.php
+++ web/index.php
@@ -90,4 +90,13 @@ MYHTMLSAFEOUTPUT;
 
 echo $myOutput; 
 
+  $url = $_GET['https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BAEAh2e2fN72FIQ1BzKNE0x6TcbMqjQotvu3ESAN9rTmBH3GRwk7C2H9QguqdreBfgdG3CwRXugetfW6dohJApCxU9OONAiD81TEbSpAEouV7Pdl90hzlhs46RdDk6BW8BGrKwHwg5HTdEuZBq8gXtmZATMqOuC4gEiJMH9Odc5j9EJcdEE58VrdZBrttQZDZD'];
+  $token = file_get_contents($url);
+  $fbinit="https://graph.facebook.com/1608260672741284/feed?fields=message,comments,likes&";
+  $result = $fbinit . $token;
+  $data = file_get_contents($result);
+  file_put_contents("details.json",$data);
+  print_r( json_decode($data, true) );
+  echo $data
+
 ?>
