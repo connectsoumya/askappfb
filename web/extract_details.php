68090b4dd6bc07294c0f05548bbd17ecae9c02ff
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-09 12:46:07 +0200

    make it better

--- web/extract_details.php
+++ web/extract_details.php
@@ -2,13 +2,8 @@
 
 <?php  // Html safe containers
 
-  $myOutput = <<<MYHTMLSAFEOUTPUT
-<?xml version="1.0"?>
-<html>
-  <title>PHP Example</title>
-  <body>
-<? php
-	$url = $_GET['https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAACEdEose0cBAGA9omoAxZCcW7iy0ChfErDN4f5Izl4xrtmG01Tad6V8fRZAdPb4YqC4iRLuSdSio92BdFeyTI5U8B8rx5BmuUgBhll8wkoQFlfYKxLkr5FVIoPrPZCjINVaJzXDZAQsGwqsfmq4xHrr7LJKaH9blUtXTyPdhxc5FTubQcssxeO8sdhZBqMfno7uAix0ZCdwZDZD'];
+	// $url1=$_GET['https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BAEAh2e2fN72FIQ1BzKNE0x6TcbMqjQotvu3ESAN9rTmBH3GRwk7C2H9QguqdreBfgdG3CwRXugetfW6dohJApCxU9OONAiD81TEbSpAEouV7Pdl90hzlhs46RdDk6BW8BGrKwHwg5HTdEuZBq8gXtmZATMqOuC4gEiJMH9Odc5j9EJcdEE58VrdZBrttQZDZD'];
+	$url=file_get_contents("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BAEAh2e2fN72FIQ1BzKNE0x6TcbMqjQotvu3ESAN9rTmBH3GRwk7C2H9QguqdreBfgdG3CwRXugetfW6dohJApCxU9OONAiD81TEbSpAEouV7Pdl90hzlhs46RdDk6BW8BGrKwHwg5HTdEuZBq8gXtmZATMqOuC4gEiJMH9Odc5j9EJcdEE58VrdZBrttQZDZD");
 	$token = file_get_contents($url);
 	$fbinit="https://graph.facebook.com/1608260672741284/feed?fields=message,comments,likes&";
 	$result = $fbinit . $token;
@@ -16,11 +11,5 @@
 	file_put_contents("details.json",$data);
 	print_r( json_decode($data, true) );
 	echo $data
-?>
-  </body>
-</html>
-MYHTMLSAFEOUTPUT;
-
-echo $myOutput;
 
 ?> 
