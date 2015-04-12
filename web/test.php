1e0495770a8405725faad93f579ce31484493dbf
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-12 17:03:20 +0200

    make it better

--- web/test.php
+++ web/test.php
@@ -20,7 +20,7 @@ $myOutput = <<<MYHTMLSAFEOUTPUT
 
 		var body= <?php  $body=file_get_contents('https://askappfb.herokuapp.com/data.txt'); ?>
 
-      FB.api('/1608260672741284/feed', 'post', {message: body , access_token: 'CAACEdEose0cBAIE88W74ZB3NG81gt2E60V5wjiJdr5fjRzwYUL4QZBKIT8iPpZAnIoLQY2ZAtpDSfrEk2NsZBFPbbRvwqo3LmRpp3U1a6pjvvqWCZC5ZAppY9akYmNrztVsZBm2kktT6AE3kVst0PkjZBA1IX1Q3tJzEb4plu01pFb4ddgk4rxyzvERoq9IeREv0Eqn8r728x9AZDZD'});
+      FB.api('/1608260672741284/feed', 'post', {message: 'body' , access_token: 'CAACEdEose0cBAIE88W74ZB3NG81gt2E60V5wjiJdr5fjRzwYUL4QZBKIT8iPpZAnIoLQY2ZAtpDSfrEk2NsZBFPbbRvwqo3LmRpp3U1a6pjvvqWCZC5ZAppY9akYmNrztVsZBm2kktT6AE3kVst0PkjZBA1IX1Q3tJzEb4plu01pFb4ddgk4rxyzvERoq9IeREv0Eqn8r728x9AZDZD'});
 		}, {scope: 'publish_actions'});
 
 
