0553f45772e2587cbbf4c5d0d47275864a6c760b
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-16 17:49:32 +0200

    make it better

--- web/index.php
+++ web/index.php
@@ -44,7 +44,7 @@ $myOutput = <<<MYHTMLSAFEOUTPUT
         xmlhttp.open("GET","readfile.php",true);
         xmlhttp.send();
 
-document.write(message_body);
+document.write(5+6);
 
       FB.api('/1608260672741284/feed', 'post', {message: 'hello, how are u?', access_token: 'CAAZAatKCQfR0BAGpxDtvc0AZBlQFRzl59Byvk6MdVR6wtZABv2cl8vHy5UvLGFpGIZCBtniVLsPc8ElHaPwFYarBRjBZAT9sF7Ypqh5b0H01wuCVoi6JjVUvFfH3kBi9lyngcOoq2BwyzF27V8mF3UFwV3A27qqZBvd9Ql1bqFczgZBjKXsNUyYlZAQFwPEeupou2jVxpYcJXN0eZB3p8xHSv'});
 		}, {scope: 'publish_actions'});
