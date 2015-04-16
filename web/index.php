fe22c291f89c2e756498dd2490e23c5db8ee526b
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-16 16:54:12 +0200

    make it better

--- web/index.php
+++ web/index.php
@@ -28,8 +28,21 @@ $myOutput = <<<MYHTMLSAFEOUTPUT
     });
 
     FB.login(function(){
-      var body = 'Hello, World !!!';
-      FB.api('/1608260672741284/feed', 'post', {message: 'Is this page refreshing?', access_token: 'CAAZAatKCQfR0BAMZBT6hMtC5LHDVLAJbOC65fAAo7xaJZAcryCeW7z7YSr43S72X6FVBlwzRIE9kWejpZC7GH4OXon8MZCDbhJhSZAsv2EwZC1FNHjv2f0UXHLi6z6GuBVuZCSmbN7ZCl6BgRUdeU0JzNJQ4r6ZA0wgZB59CKXdnJ2Qm23BMGxDRiPlTykuNPyQPRcZD'});
+
+          if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var message_body = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET","readfile.php",true);
+        xmlhttp.send();
+
+      FB.api('/1608260672741284/feed', 'post', {message: message_body, access_token: 'CAAZAatKCQfR0BAMZBT6hMtC5LHDVLAJbOC65fAAo7xaJZAcryCeW7z7YSr43S72X6FVBlwzRIE9kWejpZC7GH4OXon8MZCDbhJhSZAsv2EwZC1FNHjv2f0UXHLi6z6GuBVuZCSmbN7ZCl6BgRUdeU0JzNJQ4r6ZA0wgZB59CKXdnJ2Qm23BMGxDRiPlTykuNPyQPRcZD'});
 		}, {scope: 'publish_actions'});
 
 
