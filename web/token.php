7a7470931c0a8d04b27660819b492153c102bcfa
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-17 16:10:17 +0200

    make it better

--- web/token.php
+++ web/token.php
@@ -1,17 +1,54 @@
 <?php  
-$acc_tok_temp = file_get_contents("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BAKGIFBlskEyfencQSvmnvChVZACWqZA2o7J6IngI4rWq6kVpnGswZATnERTVTcrKtisIkHrn6ZBELFOxuL2s4mpdHGJBxFXpkOkHBRGDZBPUIDiQr7ukCzEZBZBPYkB0hpBRPjtx0qhI9mI2GEsFUc5RdvixMasMhZCOSwwdUcgYBWTBJD7BVwEv6PZBQSeZC08wZDZD");
-echo $acc_tok_temp;
-$code_url_1="https://graph.facebook.com/oauth/client_code?";
-$code_url_2="&client_secret=d95dde9374fe7d3715007c27db6a74a4&redirect_uri=https://askappfb.herokuapp.com/&client_id=1788581694700829";
-$code_url = $code_url_1 . $acc_tok_temp . $code_url_2;
-echo $code_url;
-$code_json = file_get_contents($code_url);
-$p = json_decode($code_json);
-$code = $p->code;
-echo $code;
-$acc_tok_1 = "https://graph.facebook.com/oauth/access_token?code=";
-$acc_tok_2 = "&client_id=1788581694700829&redirect_uri=https://askappfb.herokuapp.com/"
-$token_url = $acc_tok_1 . $code . $acc_tok_2;
-$token_json = file_get_contents($token_url);
-echo $token_json;
+
+require 'phpsdk4/autoload.php';
+ 
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphNodes\GraphUser;
+use Facebook\FacebookRequestException;
+ 
+FacebookSession::setDefaultApplication('APP-ID', 'APP-SECRET');
+$session = new FacebookSession($_GET['access_token']);
+ 
+try {
+  $me = (new FacebookRequest(
+    $session, 'GET', '/me'
+  ))->execute()->getGraphObject(GraphUser::className());
+  echo $me->getName();
+} catch (FacebookRequestException $e) {
+  // The Graph API returned an error
+}
+
+$scope = array('manage_pages, read_stream');
+$helper = new FacebookRedirectLoginHelper('https://www.mydomain.com/after_login.php');
+$loginUrl = $helper->getLoginUrl($scope);
+echo '<a href="' . $loginUrl . '">Login</a>';
+
+try {
+    $session = $helper->getSessionFromRedirect();
+} catch(FacebookRequestException $ex) {
+    // When Facebook returns an error
+}
+if ($session) {
+    try {
+        $user_profile = (new FacebookRequest(
+            $session, 'GET', '/me'
+        ))->execute()->getGraphObject(GraphUser::className());
+        echo "Name: " . $user_profile->getName();
+    } catch(FacebookRequestException $e) {
+        echo "Exception occured, code: " . $e->getCode();
+        echo " with message: " . $e->getMessage();
+    }
+}
+
+$request = new FacebookRequest($session, 'GET', '/PAGE-ID?fields=access_token');
+$response = $request->execute();
+$result = $response->getGraphObject()->asArray();
+$pageToken = $result['access_token'];
+$facebookSession = new FacebookSession($pageToken);
+$facebookSession->getLongLivedSession();
+$request = new FacebookRequest($session, 'GET', '/PAGE-ID?fields=access_token');
+
+echo $request;
+
 ?>
