8fc2c8b721d3deb3455160083c8909fd9f300da4
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-17 12:51:00 +0200

    make it better

--- web/index2.php
+++ web/index2.php
@@ -68,7 +68,7 @@ var a;
     FB.login(function(){
 
       
-		var token= "<?php echo $token; ?>";
+		var token= <?php echo $token; ?>;
 		alert(token);
 
       FB.api('/1608260672741284/feed', 'post', {message: message_body, access_token: 'CAAZAatKCQfR0BANzAfnuFPANjknNrLQZCn0ZBiBie097CpYSfWvb3lUU6fbv1SGZBO7yH3FjWX4YDF1z9SpjeokPhpb8MOjavrA3iWyLKI0GQYZCxNPVOXlUZCNBrg6dbQ0yZA7pMNJRNlssyraDB9o2fgwdhfUsuXMcADoiIBsZClanpsPqoyQV39apn8z5AgMZD'});
