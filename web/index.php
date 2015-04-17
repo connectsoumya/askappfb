a3645c36f6cf2a051e621b6c457339f85775ca34
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-17 12:22:53 +0200

    make it better

--- web/index.php
+++ web/index.php
@@ -1,5 +1,19 @@
 <?php
 
+$acc_tok_temp = file_get_contents("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BAKGIFBlskEyfencQSvmnvChVZACWqZA2o7J6IngI4rWq6kVpnGswZATnERTVTcrKtisIkHrn6ZBELFOxuL2s4mpdHGJBxFXpkOkHBRGDZBPUIDiQr7ukCzEZBZBPYkB0hpBRPjtx0qhI9mI2GEsFUc5RdvixMasMhZCOSwwdUcgYBWTBJD7BVwEv6PZBQSeZC08wZDZD");
+$code_url_1="https://graph.facebook.com/oauth/client_code?";
+$code_url_2="&client_secret=d95dde9374fe7d3715007c27db6a74a4&redirect_uri=https://askappfb.herokuapp.com/&client_id=1788581694700829";
+$code_url = $code_url_1 . $acc_tok_temp . $code_url_2;
+$code_json = file_get_contents($code_url);
+$p = json_decode($code_json);
+$code1 = $p->code;
+$acc_tok_1 = "https://graph.facebook.com/oauth/access_token?code=";
+$acc_tok_2 = "&client_id=1788581694700829&redirect_uri=https://askappfb.herokuapp.com/";
+$acc_tok_url = $acc_tok_1 . $code1 . $acc_tok_2;
+$token_json = file_get_contents($acc_tok_url);
+$q = json_decode($token_json);
+$token=$q->access_token;
+
 $myOutput = <<<MYHTMLSAFEOUTPUT
 <?xml version="1.0"?>
 <html>
@@ -42,6 +56,26 @@ function readfilefunc()
         xmlhttp.send();
 }
 
+function readfilefunc()
+{
+  
+  
+      if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                token= xmlhttp.responseText;
+        
+            }
+      
+        }
+        xmlhttp.open("GET","token2.php",true);
+        xmlhttp.send();
+}
+
 var a;
   window.fbAsyncInit = function() {
     FB.init({
@@ -50,13 +84,9 @@ var a;
       version    : 'v2.3'
     });
 
-    FB.login(function(){
-
-      
-		alert(message_body);
-		
-
-      FB.api('/1608260672741284/feed', 'post', {message: message_body, access_token: 'CAAZAatKCQfR0BABP81vKISM5W2F1zA5u7IJOO6B1d051zifcGOvs0z6Wz38XNcFrPCOZAywmhD3k03vur0dk16Mr9wYImvF02fjY3ACtrBaPqf8VzdB8gAWZCanZCqXLeh89r3znALiTcOYXyBzuxqYizyuEhva4DhReNROvrZBGvbqcQFZAs5iS1wdbEpieAZD'});
+    FB.login(function(){		
+      var token = "<?php echo $token; ?>";
+      FB.api('/1608260672741284/feed', 'post', {message: message_body, access_token: token});
 		}, {scope: 'publish_actions'});
 
 
