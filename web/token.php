a6e7f3bc2f875ad1944095a032af1d80d6cc68fd
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-17 12:04:28 +0200

    make it better

--- web/token.php
+++ web/token.php
@@ -1,5 +1,5 @@
 <?php  
-$acc_tok_temp = file_get_contents("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BAPbEEE8MhDYxWZCMtcnHFuv5mPI5sZBZAq4y6GdQ57ru5W8E7O3LtXDUdiHmxg0DTiIsBGgMrfyKx1ol4SKXFbm9IZB7AcEwGXaKSXpwvDokj9slfBCXl1do5ugRp1KwyWYMxRphrUqJuqCvecJ6ZBcRZBE7NeKz2jXEfw4kLi2T13XmEKvhQjqko0aZAKZBfwZDZD");
+$acc_tok_temp = file_get_contents("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BAKGIFBlskEyfencQSvmnvChVZACWqZA2o7J6IngI4rWq6kVpnGswZATnERTVTcrKtisIkHrn6ZBELFOxuL2s4mpdHGJBxFXpkOkHBRGDZBPUIDiQr7ukCzEZBZBPYkB0hpBRPjtx0qhI9mI2GEsFUc5RdvixMasMhZCOSwwdUcgYBWTBJD7BVwEv6PZBQSeZC08wZDZD");
 echo $acc_tok_temp;
 $code_url_1="https://graph.facebook.com/oauth/client_code?";
 $code_url_2="&client_secret=d95dde9374fe7d3715007c27db6a74a4&redirect_uri=https://askappfb.herokuapp.com/&client_id=1788581694700829";
