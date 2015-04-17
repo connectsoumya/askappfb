626efb90a8ce0f8070730c9a208bb6a7639c4cfb
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-04-17 11:13:21 +0200

    make it better

--- web/token.php
+++ web/token.php
@@ -1,4 +1,8 @@
 <?php  
 $acc_tok_1 = file_get_contents("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=1788581694700829&client_secret=d95dde9374fe7d3715007c27db6a74a4&fb_exchange_token=CAAZAatKCQfR0BABP81vKISM5W2F1zA5u7IJOO6B1d051zifcGOvs0z6Wz38XNcFrPCOZAywmhD3k03vur0dk16Mr9wYImvF02fjY3ACtrBaPqf8VzdB8gAWZCanZCqXLeh89r3znALiTcOYXyBzuxqYizyuEhva4DhReNROvrZBGvbqcQFZAs5iS1wdbEpieAZD");
-echo $acc_tok_1;
+$code_url_1="https://graph.facebook.com/oauth/client_code?";
+$code_url_2="&client_secret=d95dde9374fe7d3715007c27db6a74a4&redirect_uri=https://askappfb.herokuapp.com/&client_id=1788581694700829";
+$code_url=$code_url_1 . $acc_tok_1 . $code_url_2;
+$code = file_get_contents($code_url);
+echo $code;
 ?>
