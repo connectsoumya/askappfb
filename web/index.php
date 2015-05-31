2739d9abb5a071d23238cc12690ef108e9f59c4f
Soumya Shubhra Ghosh <connectsoumya@gmail.com>
2015-05-31 13:17:34 +0200

    make it better

--- web/index.php
+++ web/index.php
@@ -3,15 +3,15 @@
 // Read and decode the post request
 
 $string=file_get_contents("php://input");
-// $string='{
-//     "id": "4a11c9f1-a603-11e4-b25d-0e784dbb1c61",
-//     "content": "{\"question\":\"Is it successful?\",\"type\":\"DIRECT\",\"timestamp\":1422349376846}",
+//  $string='{
+//     "id": "4a11c9f1-a603-11e4-b25d-0e784dbb1c62",
+//     "content": "{\"question\":\"Is it suc?\",\"type\":\"DIRECT\",\"timestamp\":1422349376846}",
 //     "type": "ask",
-//     "subtype": "question",
-//     "sender": "ask",
-//     "conversation": "4a11c9f0-a603-11e4-b25d-0eesddbb1c61",
-//     "ttl": "0"
-// }';
+//      "subtype": "question",
+//      "sender": "ask",
+//      "conversation": "4a11c9f0-a603-11e4-b25d-0eesddbb1c61",
+//      "ttl": "0"
+//  }';
 $string=json_decode($string,true);
 $ask_id=$string['id'];
 $ask_content=$string['content'];
@@ -80,101 +80,8 @@ $options = array('dir' => '../');
 // Load the databases
 $q_map = Flintstone::load('q_map', $options);
 // Set keys
-$q_map->set($q_id, array('ask_con' => $ask_conversation, 'ask_id' => $ask_id, 'ask_type' => $ask_type, 'ask_sender' => $ask_sender));
-
-
-// // Create seed data
-// $seedData = array(
-//     array(
-//         'ask_con' => $ask_conversation, 
-//         'ask_id' => $ask_id,
-//         'ask_type' => $ask_type, 
-//         'ask_sender' => $ask_sender
-//     ),
-// );
-
-// // connect to mongolab
-
-// $uri = "mongodb://pritam.b.:pritam-1@ds037262.mongolab.com:37262/ask";
-// $options = array("connectTimeoutMS" => 30000);
-// $client = new MongoClient($uri,$options);
-// $db = $client->selectDB("ask");
-
-// $ask_data = $db->ask_data;
-// $ask_data->batchInsert($seedData);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// output in the browser
-
-// ob_start();
-//   $url="https://graph.facebook.com/1608260672741284/feed?fields=message,comments,likes&access_token=CAAZAatKCQfR0BANacLZBh68l9l5cJArxItfvcOp8cEzjcs2E5acFz9HU5qKwAvZCi6Dp6KbdmwxKVDvizkE6IvgpVutuTzuvAOIWgUl978v7XYghoJoTeCcMhNgLbJUQxGNeM1OpBMlvS41lFSperx6oy9fv61qptOkMCTXrB663kLsQNCDAZBZAFBbjZA7ZCY2rJzsQy9tX9snNjIobh6n";
-//   $data = file_get_contents($url);
-//   file_put_contents("details.json",$data);
-//   print_r( json_decode($data, true) );
-// ob_end_clean();
-  
-  // $url="https://graph.facebook.com/1608260672741284/feed?fields=message,comments,likes&access_token=CAAZAatKCQfR0BADrIHdqaYu4u4aiPo2Ighq3vqMmqghiAkp1L84m8CLSh6Fla70OfqfdNBHfLZBXVTFf4rlF5XjEkqVDKD3qBCBbglMwA8kOzNiCRrqj787UtCRXYgmi9e7sBrZBs1rGBQ3omZBkMCttiogdfM3MiX3SdJZBvmwarMXpKKOEo8U8pk81CjY4ZD";
-  // $data = file_get_contents($url);
-  // file_put_contents("details.json",$data);
-  // print_r( json_decode($data, true) );
-
-// $json = file_get_contents('https://askappfb.herokuapp.com/details.json');
-// $json_o = json_decode($json);
-// $i=0;
-// $a=array();
-// $b=array();
-// $questionids=array();
-// if($json_o->data!=null)
-// {
-// foreach($json_o->data as $p)
-// {
-//   // $questionids[]=$p->id;
-//   // print_r($questionids);
-
-// {if(isset($p->message))
-
-// if(isset($p->comments))
-
-// {
-// $obj1=$p->comments->data;
-// echo '<br /></b>Question: </b>';
-
-// echo $p->message.' ';
-// echo '<br /><b>Answer: </b>';
-// $i=0;
-// foreach($obj1 as $p1)
-// {
-  
-//   echo $p1->message.'<b> Votes=</b>';
-//   echo $p1->like_count.'   ';
-//   $a[$i]=$p1->message;
-//   $b[$i]=$p1->like_count;
-//   $i=$i+1;
-  
-
-  
-  
-  
-// }
-// echo '<br /><b>Best Answer: </b>';
-// echo $a[array_search(max($b), $b)].'<br />';
-// $b_reply=$a[array_search(max($b), $b)];
-
-// }
-// }
-// }
-// }
-
+$q_map->set($q_id, array('ask_con' => $ask_conversation, 'ask_id' => $ask_id, 'ask_type' => $ask_type, 'ask_sender' => $ask_sender,'ask_answer' => ''));
+$a = $q_map->get($q_id);
+print_r($a);
 
 ?>
--- web/senddata.php
+++ web/senddata.php
@@ -1,6 +1,10 @@
 <?php
 
+//Refresh session after 10 seconds
+
 header("Refresh: 10;");
+
+// Start session
 
 session_start();
 require_once 'facebook-php-sdk/autoload.php';
@@ -9,11 +13,8 @@ use Facebook\FacebookRequest;
 use Facebook\GraphUser;
 use Facebook\FacebookRequestException;
 use Facebook\FacebookRedirectLoginHelper;
-
-// Start session
-
 FacebookSession::setDefaultApplication('1788581694700829', 'd95dde9374fe7d3715007c27db6a74a4');
-$helper = new FacebookRedirectLoginHelper('https://askappfb.herokuapp.com/index2.php');
+$helper = new FacebookRedirectLoginHelper('index.php');
 $loginUrl = $helper->getLoginUrl();
 $token="CAAZAatKCQfR0BAOCMELSVBZAkkJms3usEGgQgSyShdeVWhkijtuUhDE12ZA2ZCR0awpT5hZB4Xom2lhOZCreAgGupDZCnN93ltWMJGoeBzxTBiA08r8QXlbXeS1WuRSxlny8s0a6frcZAJILzZAOni8S8ORZA4fs3oy1KoM40VAdqU5dfjYziYcF8o";
 
@@ -28,61 +29,6 @@ if ($session) {
   // Logged in
 }
 
-// Get Notification
-
-$session = new FacebookSession($token);
-$request = new FacebookRequest(
-  $session,
-  'GET',
-  '/1608260672741284/notifications'
-);
-$response = $request->execute();
-$graphObject = $response->getGraphObject();
-$album =  $graphObject->getProperty('data');
-$album_data = $album->asArray();
-foreach($album_data as $row){
-    $q_id=$row->object->id;
-    {
-        $link=$row->link;
-        $link1=explode("/",$link);
-        $link2=explode("?",$link1[5]);
-        $link3=explode("=",$link2[1]);
-        $x=(string)$link2[0];
-        $y=(string)$link3[1];
-        $comment_id=$x.'_'.$y;
-    }
-}
-
-
-// Retrieve the Reply
-
-$json = file_get_contents('https://graph.facebook.com/1608260672741284/feed?fields=message%2Ccomments%2Clikes&access_token=CAAZAatKCQfR0BANacLZBh68l9l5cJArxItfvcOp8cEzjcs2E5acFz9HU5qKwAvZCi6Dp6KbdmwxKVDvizkE6IvgpVutuTzuvAOIWgUl978v7XYghoJoTeCcMhNgLbJUQxGNeM1OpBMlvS41lFSperx6oy9fv61qptOkMCTXrB663kLsQNCDAZBZAFBbjZA7ZCY2rJzsQy9tX9snNjIobh6n&__mref=message_bubble');
-$json_o = json_decode($json);
-if($json_o->data!=null)
-{
-    foreach($json_o->data as $p)
-        {
-        if(isset($p->message))
-        if(isset($p->comments))
-            {
-            $obj1=$p->comments->data;
-            foreach($obj1 as $p1)
-                {
-                $a=$p1->id;
-                if($a==$comment_id)
-                    {
-                    $reply=$p1->message;
-                    $reply_like=$p1->like_count;
-                         
-                    }  
-                }
-            }
-        }
-}
-
-echo $reply;
-
-
 
 // Retrieve details from database
 
@@ -93,56 +39,64 @@ $options = array('dir' => '../');
 // Load the databases
 $q_map = Flintstone::load('q_map', $options);
 // Retrieve keys
-$user = $q_map->get($q_id);
+$keys=$q_map->getKeys();
 
+//Post JSON file to endpoint if best answer of any question changes
 
-// Post the JSON
-
-$urlsend='http://askappfb.herokuapp.com/test.php';//'http://smartsociety.u-hopper.com/message/';
-$ch=curl_init($urlsend);
-$answer = array(
-  'type' => $user['ask_type'] , 
-  'subtype' => "answer",
-  'content' => $reply,
-  'sender' => $user['ask_sender'],
-  'conversation' => $user['ask_con'] ,
-  'language' => '',
-  'securityToken' =>''
-  );
+$url="https://graph.facebook.com/1608260672741284/feed?fields=message%2Ccomments%2Clikes&access_token=CAAZAatKCQfR0BANacLZBh68l9l5cJArxItfvcOp8cEzjcs2E5acFz9HU5qKwAvZCi6Dp6KbdmwxKVDvizkE6IvgpVutuTzuvAOIWgUl978v7XYghoJoTeCcMhNgLbJUQxGNeM1OpBMlvS41lFSperx6oy9fv61qptOkMCTXrB663kLsQNCDAZBZAFBbjZA7ZCY2rJzsQy9tX9snNjIobh6n&__mref=message_bubble";
+ $data = file_get_contents($url);
+ $json_o = json_decode($data);
+ $a=array();
+ $b=array();
+ if($json_o->data!=null)
+ {
+ foreach($json_o->data as $p)
+ {
+	 if(isset($p->message))
+     if(isset($p->comments))
+{
+	$obj1=$p->comments->data; 
+	 $q_id=$p->id;
+	 	 if (in_array($q_id, $keys))
+	 {
+		 $user=$q_map->get($q_id);
+		 $answer=$user['ask_answer'];
+		 $i=0;
+ foreach($obj1 as $p1)
+ {
+    $a[$i]=$p1->message;
+   $b[$i]=$p1->like_count;
+   $i=$i+1;
+  }
+ 
+ $b_reply=$a[array_search(max($b), $b)];
+ //echo "the answer is  ".$b_reply.'<br />';
+		if(strcmp($answer,$b_reply)!=0)
+		{
+			$urlsend='test.php';//'http://smartsociety.u-hopper.com/message/';
+			$q_map->set($q_id, array('ask_con' => $user['ask_con'], 'ask_id' =>$user['ask_id'] , 'ask_type' => $user['ask_type'], 'ask_sender' => $user['ask_sender'],'ask_answer' => $b_reply));
+			$ch=curl_init($urlsend);
+			$answer = array(
+			  'type' => $user['ask_type'] , 
+			  'subtype' => "answer",
+			  'content' => $b_reply,
+			  'sender' => $user['ask_sender'],
+			  'conversation' => $user['ask_con'] ,
+			  'language' => '',
+			  'securityToken' =>''
+			);
 
 print_r($answer);
-
 $answer_json=json_encode($answer);
 curl_setopt($ch, CURLOPT_URL, $urlsend);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $answer_json);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 $result=curl_exec($ch);
-
-// $answer = array(
-//   'type' => $user['ask_type'] , 
-//   'subtype' => "answer",
-//   'content' => $reply,
-//   'sender' => $user['ask_sender'],
-//   'conversation' => $user['ask_con'] ,
-//   'language' => '',
-//   'securityToken' =>''
-//   );
-// $answer_json=json_encode($answer);
-// print_r($answer);
-// # Form our options
-// $opts = array('http' =>
-//     array(
-//         'method'  => 'POST',
-//         'header'  => 'Content-type: application/json',
-//         'content' => $answer_json
-//     )
-// );
-
-// # Create the context
-// $context = stream_context_create($opts);
-
-// # Get the response (you can use this for GET)
-// $result = file_get_contents('http://askappfb.herokuapp.com/test.php', false, $context);
+			 }
+			}
+	 }
+ }
+ }
 
 ?>
